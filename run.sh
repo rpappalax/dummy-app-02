fea3cabb9b9ea3bce770538531bb1bd9b703c224
Richard Pappalardo <rpappalax@gmail.com>
2017-11-14 13:26:32 -0800

    fix: circle

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
  #pytest --driver Remote --host selenium-firefox --port 4444 --capability browserName firefox test-ff.py
  #jpytest --driver Remote --host 127.0.0.1 --port 4444 --capability browserName firefox test-ff.py
  #pytest --driver Remote --host 0.0.0.0 --port 4444 --capability browserName firefox test-ff.py
-echo "Peter is cool #1"
+echo "Peter is cool #2"
