bc9bc10d5c682119b78c109f4ea9f979f4e82079
Peter deHaan <peter@deseloper.com>
2017-11-21 14:32:19 -0800

    Shrinken the circleci/config.yml

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
  #pytest --driver Remote --host selenium-firefox --port 4444 --capability browserName firefox test-ff.py
- #jpytest --driver Remote --host 127.0.0.1 --port 4444 --capability browserName firefox test-ff.py
+ #pytest --driver Remote --host 127.0.0.1 --port 4444 --capability browserName firefox test-ff.py
  #pytest --driver Remote --host 0.0.0.0 --port 4444 --capability browserName firefox test-ff.py
-echo "Peter is cool #2"
+
+echo "sdonner is cool #2"
